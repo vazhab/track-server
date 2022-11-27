754837d5c894e9985c7ae313608e15b6f54b8eef
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-27 11:29:51 +0400

    MongoDB setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { SECRET_MONGO_URI } = require('./constant');
 
 const app = express();
+
+const mongoUri = SECRET_MONGO_URI;
+
+mongoose.connect(mongoUri);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hi There!');
