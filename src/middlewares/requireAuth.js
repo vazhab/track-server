0c98ee98b9b1630b736c76eb51949fdd3d11ae01
Vazha Bichiashvili <vazha.bichiashvili@gmail.com>
2022-11-28 13:14:55 +0400

    authentication logic

---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+const { KEY } = require('../constants');
+
+module.exports = (req, res, next) => {
+    const { authorization } = req.headers;
+    //authorization === 'Bearer a,mfns,fbskdhfnlksjdf;lskdf;ls'
+    if (!authorization) {
+        return res.status(401).send({ error: 'You must be logged in.' });
+    }
+
+    const token = authorization.replace('Bearer ', '');
+    jwt.verify(token, KEY, async (err, payload) => {
+        if (err) {
+            return res.status(401).send({ error: 'You must be logged in.' });
+        }
+
+        const { userId } = payload;
+
+        const user = await User.findById(userId);
+        req.user = user;
+        next();
+    });
+}; 
